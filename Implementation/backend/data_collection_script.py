f98a57e30a9134b1409ea4fe247dfb63eca48182
krystianjp <krystianjp97@gmail.com>
2026-02-09 16:49:18 +0000

    exporting all data to csv

--- Implementation/backend/data_collection_script.py
+++ Implementation/backend/data_collection_script.py
@@ -1,12 +1,13 @@
 import requests
-import random
 import time
+import pandas as pd
+
 
 URL = "https://ballchasing.com/api/replays/"
 TOKEN = "weVNZnKyOEu3PeMbGybQ8SPovXIUkQxAXguy6fDM"
 
-DATA_STRUCTURE = {"boost":["bpm", "amount_used_while_supersonic", "count_collected_small", "count_collected_big", "percent_zero_boost", "percent_boost_0_25", "percent_full_boost", "count_stolen_big"],
-    "movement":["avg_speed", "percent_supersonic_speed", "percent_ground", "percent_low_air", "percent_high_air"], "positioning": ["percent_most_back", "percent_most_forward", "percent_closest_to_ball", "percent_farthest_from_ball", "percent_behind_ball", "percent_infront_ball", "percent_defensive_third", "percent_neutral_third", "percent_offensive_third"], "demo":["inflicted", "taken"]}
+DATA_STRUCTURE = {"core": ["shots", "goals", "saves", "assists", "shooting_percentage"],"boost":["bpm", "amount_used_while_supersonic", "count_collected_small", "count_collected_big", "percent_zero_boost", "percent_boost_0_25", "percent_full_boost", "count_stolen_big"],
+    "movement":["avg_speed_percentage", "percent_supersonic_speed", "percent_ground", "percent_low_air", "percent_high_air"], "positioning": ["percent_most_back", "percent_most_forward", "percent_closest_to_ball", "percent_farthest_from_ball", "percent_behind_ball", "percent_infront_ball", "percent_defensive_third", "percent_neutral_third", "percent_offensive_third"], "demo":["inflicted", "taken"]}
 
 def get_replay(id, token=TOKEN):
     time.sleep(0.5)
@@ -20,7 +21,7 @@ def get_replay(id, token=TOKEN):
 
 def get_player_replays(id, platform, count=10, token=TOKEN):
     r = requests.get(URL, headers={
-                      'Authorization': token}, params={'player-id': f"{platform}:{id}", "count": count})
+                      'Authorization': token}, params={'player-id': f"{platform}:{id}", "count": count, "playlist": "ranked-standard"})
 
     if r.status_code == 200:
         data =  r.json()
@@ -30,15 +31,21 @@ def get_player_replays(id, platform, count=10, token=TOKEN):
         r.raise_for_status()
 
 def find_average_stats(players, replay_count=1):
-    stats = {}
+    rows = []
     for player in players:
         player_replays = get_player_replays(
             player['player_id'], player['platform'], count=replay_count)
-        stats[player['player_name']] = average_stats(player_replays, player['player_id'])
-    return stats
+        player_data = average_stats(player_replays, player['player_id'])
+        row = {"rank": player['rank']}
+        for category in DATA_STRUCTURE:
+            for j,stat in enumerate(DATA_STRUCTURE[category]):
+                row[f"{category}_{stat}"] = player_data[category][j]
+        rows.append(row)
+
+    return rows
 
 def average_stats(data, player_id):
-    totals = {"boost": [0,0,0,0,0,0,0,0], "movement": [0,0,0,0,0], "positioning": [0,0,0,0,0,0,0,0,0], "demo": [0,0]}
+    totals = {"core": [0,0,0,0,0],"boost": [0,0,0,0,0,0,0,0], "movement": [0,0,0,0,0], "positioning": [0,0,0,0,0,0,0,0,0], "demo": [0,0]} # 6th core value is assist to goal ratio
     count = 0
     for replay in data['list']:
         replay_data =get_replay(replay['id'])
@@ -46,19 +53,31 @@ def average_stats(data, player_id):
         if replay_data['overtime']:
             overtime = replay_data['overtime_seconds'] / 60
         replay_duration = 5 + overtime
+
         players = replay_data['blue']['players'] + replay_data['orange']['players']
 
+        blue_goals = replay_data["blue"]['stats']['core']["goals"]
+        orange_goals = replay_data["orange"]['stats']['core']["goals"]
 
-        for player in players:
+        for i,player in enumerate(players):
             if player['id']['id'] == player_id:
 
+                if i <= len(players)/2 - 1: # blue
+                    goals = blue_goals
+                else:
+                    goals = orange_goals
+
                 for category in DATA_STRUCTURE:
-                    for stat in DATA_STRUCTURE[category]:
+                    for j,stat in enumerate(DATA_STRUCTURE[category]):
                         # some require standardising
-                        if stat in ["amount_used_while_supersonic", "count_collected_small", "count_collected_big", "count_stolen_big", "inflicted", "taken"]:
-                            totals[category][DATA_STRUCTURE[category].index(stat)] += player['stats'][category][stat] / replay_duration
+                        if stat in ["amount_used_while_supersonic", "count_collected_small", "count_collected_big", "count_stolen_big", "inflicted", "taken", "saves", "shots"]:
+                            totals[category][j] += player['stats'][category][stat] / replay_duration
+                        elif stat == "goals":
+                            totals["core"][1] += 100 / (len(players)/2) if goals == 0 else player['stats'][category][stat] * 100 / goals # average if no goals
+                        elif stat == "assists":
+                            totals["core"][3] += 100 / (len(players)/2) if goals == 0 else player['stats'][category][stat] * 100 / goals
                         else:
-                            totals[category][DATA_STRUCTURE[category].index(stat)] += player['stats'][category][stat]
+                            totals[category][j] += player['stats'][category][stat]
 
                 count += 1
     if count == 0:
@@ -66,8 +85,8 @@ def average_stats(data, player_id):
     
     # divide by count
     for category in DATA_STRUCTURE:
-        for stat in DATA_STRUCTURE[category]:
-            totals[category][DATA_STRUCTURE[category].index(stat)] = totals[category][DATA_STRUCTURE[category].index(stat)] / count
+        for i,stat in enumerate(DATA_STRUCTURE[category]):
+            totals[category][i] = int(totals[category][i] * 1000 / count) / 1000
 
     return totals
 
@@ -89,14 +108,22 @@ def test_script(rank, count, each_player_count):
                     players_list.append({
                         'player_name': player['name'],
                         'player_id': player_id,
-                        'platform': player['id']['platform'],})
-                    
+                        'platform': player['id']['platform'],
+                        'rank': rank
+                        })
+     
         return find_average_stats(players_list, each_player_count)
     else:
         r.raise_for_status()
 
+ranks = ["platinum-1", "platinum-2", "platinum-3", "diamond-1", "diamond-2", "diamond-3", "champion-1", "champion-2", "champion-3", "grand-champion-1", "grand-champion-2", "grand-champion-3"]
 
+rows = []
 
-# print(test_script(rank="champion-3", count=1, each_player_count=4))
-print(get_replay("0aaba5c2-ba9e-4a2d-8aac-b6a06e934eb6"))
+for rank in ranks:
+    rows += test_script(rank=rank, count=1, each_player_count=6)
+
+df = pd.DataFrame(rows)
+df.to_csv("player_stats.csv", index=True)
+# print(get_replay("0aaba5c2-ba9e-4a2d-8aac-b6a06e934eb6"))
 
