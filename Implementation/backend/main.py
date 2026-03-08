a5c27ede0ec3e6855386e4cd09dc330c8e7f69e6
Krystian Procowski <kxp314@student.bham.ac.uk>
2026-03-08 20:53:52 +0000

    removed trailing slash

--- Implementation/backend/main.py
+++ Implementation/backend/main.py
@@ -32,7 +32,7 @@ app = FastAPI()
 origins = [
     "http://localhost:5173", 
     "http://127.0.0.1:5173",
-    "https://rl-replay-analyser.vercel.app/"
+    "https://rl-replay-analyser.vercel.app"
 ]
 
 RRROCKET_PATH = "./rrrocket.exe"
